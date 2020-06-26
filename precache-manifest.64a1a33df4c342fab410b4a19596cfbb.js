519f7c3e94ffb93398c253f7b127ef63356eb415
Anand <anand.sg@fleximc.com>
2020-06-26 13:27:18 +0530

    Updates

---
+++ precache-manifest.64a1a33df4c342fab410b4a19596cfbb.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "45b324e47b083aed4659695407aedf9f",
+    "url": "/task/index.html"
+  },
+  {
+    "revision": "6bb41e38bc947d2431a8",
+    "url": "/task/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "6bb41e38bc947d2431a8",
+    "url": "/task/static/js/2.0e65bd75.chunk.js"
+  },
+  {
+    "revision": "fdd30b2ab44ae896589b798d411d5c2c",
+    "url": "/task/static/js/2.0e65bd75.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7cf2828451addd44a9ac",
+    "url": "/task/static/js/main.3fc41a53.chunk.js"
+  },
+  {
+    "revision": "94f1d39390e4100900a4",
+    "url": "/task/static/js/runtime-main.ed87bbea.js"
+  }
+]);
